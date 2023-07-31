be49454bcfa305b30e0e274710012fcd7781f038
Hamad <hamadullah16@gmail.com>
2023-07-31 16:13:18 +0500

    User interface modifications

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,25 +13,27 @@ function Form() {
     setMessage("");
   }
   return (
-    <div className={`container-sm p-3 d-flex justify-content-center ${Styles.main}`} >
+    <div className={`container-md p-3 d-flex justify-content-center ${Styles.main}`} >
       <form className={`${Styles.form} m-4 p-3 rounded shadow`} onSubmit={submitHandler}>
-        <h1 className='text-center'>Contact me</h1>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">Name</label>
-          <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="name" required />
-        </div>
+        <h1 className='text-center fs-3 mt-3'>Contact me</h1>
+        <div className='px-3 align-items-center'>
+          <div className="mb-3 mt-5">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="name" required />
+          </div>
 
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email address</label>
-          <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="email" required />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="message" className="form-label">Message</label>
-          <textarea value={message} onChange={(e) => setMessage(e.target.value)} type="text" className="form-control" id="message" required />
-        </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email address</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="email" required />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label">Message</label>
+            <textarea value={message} onChange={(e) => setMessage(e.target.value)} type="text" className="form-control" id="message" required />
+          </div>
 
-        <button type="submit" className={`btn ${Styles.button} shadow`}>Submit</button>
+          <button type="submit" className={`btn ${Styles.button} shadow`}>Submit</button>
+        </div>
       </form>
     </div>
   )
--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -5,21 +5,21 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const project = [
-    { name: "Express API", image: "/Thumbnail_Express.jpg", description: "Express.js API with CRUD features. MongoDB Database. HTML, CSS, EJS, Bootstrap5 Frontend.", release: "", repo: "https://github.com/HamadUllah16/React-Resume-App", deployed: true },
+    { name: "Express API", image: "/Thumbnail_Express.jpg", description: "Express.js API with CRUD features. MongoDB Database. HTML, CSS, EJS, Bootstrap5 Frontend.", release: "", repo: "https://github.com/HamadUllah16/ExpressAPI", deployed: true },
     { name: "Portfolio App", image: "/Thumbnail_Resume.jpg", description: "Single page portfolio application built in React & Bootstrap.", release: "https://resume-react-app.vercel.app", repo: "https://github.com/HamadUllah16/React-Resume-App", deployed: true },
     { name: "Title", image: "/NoThumbnail.jpg", description: "Description", release: "", repo: "", deployed: false }
 ]
 
 function ProjectContainer() {
 
-    const renderProjects = project.map((item, index) => (<div key={index} className={`${Styles.flexItem} mb-5 mt-2 text-dark px-2 rounded bg-light shadow text-light`}>
-        <img src={item.image} className={`${Styles.projectImage} img-thumbnail mt-2`} alt='project banner' />
-        <h1 className='fs-3 py-1 text-dark text-center'>{item.name}</h1>
-        <p className='text-dark'>{item.description}</p>
-        <div> {item.deployed ? <div className='d-inline'><p className='d-inline fw-bold'>Deployment:</p> <a href={item.repo} target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} style={{color:"#000000"}} /></a> </div> : <p className='text-dark'>Not deployed yet</p>}</div>
-        {item.release==="" ? <p className='fw-bold'>Not Released Yet!</p> : <div><a className={`${Styles.a} text-dark`} href={item.release} target='_blank' rel='noreferrer'><b>Released:</b> Read more <FontAwesomeIcon icon={faArrowRight} /></a></div> }
-        
-        
+    const renderProjects = project.map((item, index) => (<div key={index} className={`${Styles.flexItem} mb-5 text-dark rounded bg-light shadow text-light`}>
+        <div><img src={item.image} className={`${Styles.projectImage} img-fluid`} alt='project banner' /></div>
+        <div className='px-2'>
+            <h1 className='fs-3 py-1 text-dark text-center'>{item.name}</h1>
+            <p className='text-dark'>{item.description}</p>
+            <div> {item.deployed ? <div className='d-inline'><p className='d-inline fw-bold'>Deployment:</p> <a href={item.repo} target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} style={{ color: "#000000" }} /></a> </div> : <p className='text-dark'>Not deployed yet</p>}</div>
+            {item.release === "" ? <p className='fw-bold'>Not Released Yet!</p> : <div><a className={`${Styles.a} text-dark`} href={item.release} target='_blank' rel='noreferrer'><b>Released:</b> Read more <FontAwesomeIcon icon={faArrowRight} /></a></div>}
+        </div>
 
     </div>))
     return (

28fdbc447686cc566cc3eced2ea7b6a44fe670ba
Hamad <hamadullah16@gmail.com>
2023-08-01 05:18:11 +0500

    Added new project in the project panel

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -6,14 +6,14 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const project = [
     { name: "Express API", image: "/Thumbnail_Express.jpg", description: "Express.js API with CRUD features. MongoDB Database. HTML, CSS, EJS, Bootstrap5 Frontend.", release: "", repo: "https://github.com/HamadUllah16/ExpressAPI", deployed: true },
-    { name: "Portfolio App", image: "/Thumbnail_Resume.jpg", description: "Single page portfolio application built in React & Bootstrap.", release: "https://resume-react-app.vercel.app", repo: "https://github.com/HamadUllah16/React-Resume-App", deployed: true },
-    { name: "Title", image: "/NoThumbnail.jpg", description: "Description", release: "", repo: "", deployed: false }
+    { name: "Portfolio App", image: "/Thumbnail_Resume.jpg", description: "Single page portfolio application built in React & Bootstrap.", release: "https://hamad-portfolio.vercel.app", repo: "https://github.com/HamadUllah16/React-Resume-App", deployed: true },
+    { name: "ToDos App", image: "/todo-app.png", description: "A simple ToDos application built in React.", release: "https://hamad-todo.vercel.app", repo: "https://github.com/HamadUllah16/ToDo-App", deployed: true }
 ]
 
 function ProjectContainer() {
 
     const renderProjects = project.map((item, index) => (<div key={index} className={`${Styles.flexItem} mb-5 text-dark rounded bg-light shadow text-light`}>
-        <div><img src={item.image} className={`${Styles.projectImage} img-fluid`} alt='project banner' /></div>
+        <div><a href={item.release} target='_blank' rel='noreferrer'><img src={item.image} className={`${Styles.projectImage} img-fluid`} alt='project banner' /></a></div>
         <div className='px-2'>
             <h1 className='fs-3 py-1 text-dark text-center'>{item.name}</h1>
             <p className='text-dark'>{item.description}</p>
